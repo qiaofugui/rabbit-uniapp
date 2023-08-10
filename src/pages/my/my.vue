be58e934f174737841c99ad082c0915873be14d0
qiaoronggui <903111082@qq.com>
2023-08-10 17:24:37 +0800

    请求和上传文件拦截器

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
-//
+import { useMemberStore } from '@/stores'
+import '@/utils/http'
+
+const memberStore = useMemberStore()
+// 测试请求
+const getData = () => {
+  uni.request({
+    method: 'GET',
+    url: '/home/banner',
+    header: {},
+  })
+}
 </script>
 
 <template>
   <view class="my">my</view>
+  <view class="my">
+    <view>会员信息：{{ memberStore.profile }}</view>
+    <button
+      @tap="
+        memberStore.setProfile({
+          nickname: '小兔鲜',
+          token: '12345',
+        })
+      "
+      size="mini"
+      plain
+      type="primary"
+    >
+      保存用户信息
+    </button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">
+      清理用户信息
+    </button>
+    <button @tap="getData" size="mini" plain type="primary">测试请求</button>
+  </view>
 </template>
 
 <style lang="scss">
