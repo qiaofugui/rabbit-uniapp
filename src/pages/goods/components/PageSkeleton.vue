819a70898bb528995c13d49fe3feb96760d842b3
qiaoronggui <903111082@qq.com>
2023-08-11 16:06:38 +0800

    商品详情 骨架屏

---
+++ src/pages/goods/components/PageSkeleton.vue
@@ -0,0 +1,182 @@
+<template name="skeleton">
+  <view class="sk-container">
+    <scroll-view scroll-y="true" class="viewport" enable-back-to-top="true">
+      <view class="goods">
+        <view class="preview">
+          <swiper circular="true" current="0" autoplay="false">
+            <swiper-item
+              style="position: absolute; width: 100%; height: 100%; transform: translate(0%, 0px) translateZ(0px);">
+              <image mode="aspectFill" class="sk-image"></image>
+            </swiper-item>
+          </swiper>
+          <view class="indicator">
+            <text class="current sk-transparent sk-opacity">1</text>
+            <text class="split sk-transparent sk-opacity">/</text>
+            <text class="total sk-transparent sk-opacity">5</text>
+          </view>
+        </view>
+        <view class="meta">
+          <view class="price">
+            <text class="symbol sk-transparent sk-opacity">¥</text>
+            <text class="number sk-transparent sk-text-14-2857-106 sk-text">166.00</text>
+          </view>
+          <view class="name ellipsis sk-transparent sk-text-14-2857-774 sk-text">极光限定 珠光蓝珐琅锅</view>
+          <view class="desc"></view>
+        </view>
+        <view class="action">
+          <view class="item arrow sk-pseudo sk-pseudo-circle">
+            <text class="label sk-transparent sk-text-14-2857-886 sk-text">选择</text>
+            <text class="text ellipsis sk-transparent sk-text-14-2857-538 sk-text"> 请选择商品规格 </text>
+          </view>
+          <view class="item arrow sk-pseudo sk-pseudo-circle">
+            <text class="label sk-transparent sk-text-14-2857-740 sk-text">送至</text>
+            <text class="text ellipsis sk-transparent sk-text-14-2857-720 sk-text"> 请选择收获地址 </text>
+          </view>
+          <view class="item arrow sk-pseudo sk-pseudo-circle">
+            <text class="label sk-transparent sk-text-14-2857-418 sk-text">服务</text>
+            <text class="text ellipsis sk-transparent sk-text-14-2857-571 sk-text"> 无忧退 快速退款 免费包邮 </text>
+          </view>
+        </view>
+      </view>
+    </scroll-view>
+    <view class="toolbar" style="padding-bottom:0px">
+      <view class="icons">
+        <button class="icons-button sk-transparent sk-button sk-pseudo sk-pseudo-circle">
+          <text class="icon-heart sk-pseudo sk-pseudo-circle"></text>收藏</button>
+        <button class="icons-button sk-transparent sk-button sk-pseudo sk-pseudo-circle" open-type="contact">
+          <text class="icon-handset sk-pseudo sk-pseudo-circle"></text>客服 </button>
+        <navigator class="icons-button sk-transparent" open-type="switchTab">
+          <text class="icon-cart sk-pseudo sk-pseudo-circle"></text>购物车
+        </navigator>
+      </view>
+      <view class="buttons">
+        <view class="addcart sk-transparent sk-text-31-9444-825 sk-text" style="background-position-x: 50%;"> 加入购物车
+        </view>
+        <view class="buynow sk-transparent sk-text-31-9444-587 sk-text" style="background-position-x: 50%;"> 立即购买 </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<style>
+/* #ifdef H5 || APP-PLUS */
+/* 修复 H5 端骨架屏样式异常 */
+@import '@/components/styles/XtxSwiper.scss';
+
+/* #endif */
+
+.sk-transparent {
+  color: transparent !important;
+}
+
+.sk-opacity {
+  opacity: 0 !important;
+}
+
+.sk-text-14-2857-106 {
+  background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+  background-size: 100% 78.4000rpx;
+  position: relative !important;
+}
+
+.sk-text {
+  background-origin: content-box !important;
+  background-clip: content-box !important;
+  background-color: transparent !important;
+  color: transparent !important;
+  background-repeat: repeat-y !important;
+}
+
+.sk-text-14-2857-774 {
+  background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+  background-size: 100% 44.8000rpx;
+  position: relative !important;
+}
+
+.sk-text-14-2857-886 {
+  background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+  background-size: 100% 36.4000rpx;
+  position: relative !important;
+}
+
+.sk-text-14-2857-538 {
+  background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+  background-size: 100% 36.4000rpx;
+  position: relative !important;
+}
+
+.sk-text-14-2857-740 {
+  background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+  background-size: 100% 36.4000rpx;
+  position: relative !important;
+}
+
+.sk-text-14-2857-720 {
+  background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+  background-size: 100% 36.4000rpx;
+  position: relative !important;
+}
+
+.sk-text-14-2857-418 {
+  background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+  background-size: 100% 36.4000rpx;
+  position: relative !important;
+}
+
+.sk-text-14-2857-571 {
+  background-image: linear-gradient(transparent 14.2857%, #EEEEEE 0%, #EEEEEE 85.7143%, transparent 0%) !important;
+  background-size: 100% 36.4000rpx;
+  position: relative !important;
+}
+
+.sk-text-31-9444-825 {
+  background-image: linear-gradient(transparent 31.9444%, #EEEEEE 0%, #EEEEEE 68.0556%, transparent 0%) !important;
+  background-size: 100% 72.0000rpx;
+  position: relative !important;
+}
+
+.sk-text-31-9444-587 {
+  background-image: linear-gradient(transparent 31.9444%, #EEEEEE 0%, #EEEEEE 68.0556%, transparent 0%) !important;
+  background-size: 100% 72.0000rpx;
+  position: relative !important;
+}
+
+.sk-button {
+  color: #EFEFEF !important;
+  background: #EFEFEF !important;
+  border: none !important;
+  box-shadow: none !important;
+}
+
+.sk-image {
+  background: #EFEFEF !important;
+}
+
+.sk-pseudo::before,
+.sk-pseudo::after {
+  background: #EFEFEF !important;
+  background-image: none !important;
+  color: transparent !important;
+  border-color: transparent !important;
+}
+
+.sk-pseudo-rect::before,
+.sk-pseudo-rect::after {
+  border-radius: 0 !important;
+}
+
+.sk-pseudo-circle::before,
+.sk-pseudo-circle::after {
+  border-radius: 50% !important;
+}
+
+.sk-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: transparent;
+}
+</style>
--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -5,6 +5,7 @@ import { onLoad } from '@dcloudio/uni-app'
 import type { GoodsResult } from '@/types/goods'
 import { ref } from 'vue'
 
+import PageSkeleton from './components/PageSkeleton'
 import AddressPanel from './components/AddressPanel'
 import ServicePanel from './components/ServicePanel'
 
@@ -23,14 +24,12 @@ const getGoodsDetailData = async () => {
   goods.value = res.result
 }
 
+const isLoading = ref(false)
 // 页面加载
 onLoad(async () => {
-  uni.showLoading({
-    title: '加载中',
-    mask: true
-  })
+  isLoading.value = true
   await getGoodsDetailData()
-  uni.hideLoading();
+  isLoading.value = false
 })
 
 // 轮播图变化时
@@ -64,101 +63,105 @@ const openPopup = (name: typeof popupName.value) => {
 </script>
 
 <template>
-  <scroll-view scroll-y class="viewport">
-    <!-- 基本信息 -->
-    <view class="goods">
-      <!-- 商品主图 -->
-      <view class="preview">
-        <swiper circular @change="onChange">
-          <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" @tap="onTapImage(item)" />
-          </swiper-item>
-        </swiper>
-        <view class="indicator">
-          <text class="current">{{ currentIndex + 1 }}</text>
-          <text class="split">/</text>
-          <text class="total">{{ goods?.mainPictures.length }}</text>
-        </view>
-      </view>
+  <PageSkeleton v-if="isLoading" />
 
-      <!-- 商品简介 -->
-      <view class="meta">
-        <view class="price">
-          <text class="symbol">¥</text>
-          <text class="number">{{ goods?.price }}</text>
-        </view>
-        <view class="name ellipsis">{{ goods?.name }}</view>
-        <view class="desc">{{ goods?.desc }}</view>
-      </view>
-
-      <!-- 操作面板 -->
-      <view class="action">
-        <view class="item arrow">
-          <text class="label">选择</text>
-          <text class="text ellipsis"> 请选择商品规格 </text>
-        </view>
-        <view class="item arrow" @tap="openPopup('address')">
-          <text class="label">送至</text>
-          <text class="text ellipsis"> 请选择收获地址 </text>
-        </view>
-        <view class="item arrow" @tap="openPopup('service')">
-          <text class="label">服务</text>
-          <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
-        </view>
-      </view>
-    </view>
-
-    <!-- 商品详情 -->
-    <view class="detail panel">
-      <view class="title">
-        <text>详情</text>
-      </view>
-      <view class="content">
-        <view class="properties">
-          <!-- 属性详情 -->
-          <view class="item" v-for="item in goods?.details.properties" :key="item.name">
-            <text class="label">{{ item.name }}</text>
-            <text class="value">{{ item.value }}</text>
+  <view v-else>
+    <scroll-view scroll-y class="viewport">
+      <!-- 基本信息 -->
+      <view class="goods">
+        <!-- 商品主图 -->
+        <view class="preview">
+          <swiper circular @change="onChange">
+            <swiper-item v-for="item in goods?.mainPictures" :key="item">
+              <image mode="aspectFill" :src="item" @tap="onTapImage(item)" />
+            </swiper-item>
+          </swiper>
+          <view class="indicator">
+            <text class="current">{{ currentIndex + 1 }}</text>
+            <text class="split">/</text>
+            <text class="total">{{ goods?.mainPictures.length }}</text>
           </view>
         </view>
-        <!-- 图片详情 -->
-        <image mode="widthFix" v-for="item in goods?.details.pictures" :key="item" :src="item"></image>
-      </view>
-    </view>
 
-    <!-- 同类推荐 -->
-    <view class="similar panel">
-      <view class="title">
-        <text>同类推荐</text>
-      </view>
-      <view class="content">
-        <navigator v-for="item in goods?.similarProducts" :key="item.id" class="goods" hover-class="none"
-          :url="`/pages/goods/goods?id=${item.id}}}`">
-          <image class="image" mode="aspectFill" :src="item.picture"></image>
-          <view class="name ellipsis">{{ item.name }}</view>
+        <!-- 商品简介 -->
+        <view class="meta">
           <view class="price">
             <text class="symbol">¥</text>
-            <text class="number">{{ item.price }}</text>
+            <text class="number">{{ goods?.price }}</text>
           </view>
+          <view class="name ellipsis">{{ goods?.name }}</view>
+          <view class="desc">{{ goods?.desc }}</view>
+        </view>
+
+        <!-- 操作面板 -->
+        <view class="action">
+          <view class="item arrow">
+            <text class="label">选择</text>
+            <text class="text ellipsis"> 请选择商品规格 </text>
+          </view>
+          <view class="item arrow" @tap="openPopup('address')">
+            <text class="label">送至</text>
+            <text class="text ellipsis"> 请选择收获地址 </text>
+          </view>
+          <view class="item arrow" @tap="openPopup('service')">
+            <text class="label">服务</text>
+            <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
+          </view>
+        </view>
+      </view>
+
+      <!-- 商品详情 -->
+      <view class="detail panel">
+        <view class="title">
+          <text>详情</text>
+        </view>
+        <view class="content">
+          <view class="properties">
+            <!-- 属性详情 -->
+            <view class="item" v-for="item in goods?.details.properties" :key="item.name">
+              <text class="label">{{ item.name }}</text>
+              <text class="value">{{ item.value }}</text>
+            </view>
+          </view>
+          <!-- 图片详情 -->
+          <image mode="widthFix" v-for="item in goods?.details.pictures" :key="item" :src="item"></image>
+        </view>
+      </view>
+
+      <!-- 同类推荐 -->
+      <view class="similar panel">
+        <view class="title">
+          <text>同类推荐</text>
+        </view>
+        <view class="content">
+          <navigator v-for="item in goods?.similarProducts" :key="item.id" class="goods" hover-class="none"
+            :url="`/pages/goods/goods?id=${item.id}}}`">
+            <image class="image" mode="aspectFill" :src="item.picture"></image>
+            <view class="name ellipsis">{{ item.name }}</view>
+            <view class="price">
+              <text class="symbol">¥</text>
+              <text class="number">{{ item.price }}</text>
+            </view>
+          </navigator>
+        </view>
+      </view>
+    </scroll-view>
+
+    <!-- 用户操作 -->
+    <view class="toolbar" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }">
+      <view class="icons">
+        <button class="icons-button"><text class="icon-heart"></text>收藏</button>
+        <button class="icons-button" open-type="contact">
+          <text class="icon-handset"></text>客服
+        </button>
+        <navigator class="icons-button" url="/pages/cart/cart" open-type="switchTab">
+          <text class="icon-cart"></text>购物车
         </navigator>
       </view>
-    </view>
-  </scroll-view>
-
-  <!-- 用户操作 -->
-  <view class="toolbar" :style="{ paddingBottom: safeAreaInsets?.bottom + 'px' }">
-    <view class="icons">
-      <button class="icons-button"><text class="icon-heart"></text>收藏</button>
-      <button class="icons-button" open-type="contact">
-        <text class="icon-handset"></text>客服
-      </button>
-      <navigator class="icons-button" url="/pages/cart/cart" open-type="switchTab">
-        <text class="icon-cart"></text>购物车
-      </navigator>
-    </view>
-    <view class="buttons">
-      <view class="addcart"> 加入购物车 </view>
-      <view class="buynow"> 立即购买 </view>
+      <view class="buttons">
+        <view class="addcart"> 加入购物车 </view>
+        <view class="buynow"> 立即购买 </view>
+      </view>
     </view>
   </view>
 
