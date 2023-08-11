906153ce4bdc17eaeaf119c633277c3dbef798c5
qiaoronggui <903111082@qq.com>
2023-08-11 11:14:18 +0800

    商品分类 二级分类渲染

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { BannerItem } from '@/types/home';
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { getHomeBannerAPI } from '@/services/home';
 import { onLoad } from '@dcloudio/uni-app';
 import { getCategoryTopAPI } from '@/services/category'
@@ -21,9 +21,15 @@ const getCategoryTopData = async () => {
   categoryList.value = res.result
 }
 
+// 页面加载
 onLoad(() => {
   getBannerData()
   getCategoryTopData()
+})
+
+// 提取二级分类数据
+const subCategoryList = computed(() => {
+  return categoryList.value[activeIndex.value]?.children || []
 })
 </script>
 
@@ -48,18 +54,18 @@ onLoad(() => {
         <!-- 焦点图 -->
         <XtxSwiper class="banner" :list="bannerList" />
         <!-- 内容区域 -->
-        <view class="panel" v-for="item in 3" :key="item">
+        <view class="panel" v-for="item in subCategoryList" :key="item.id">
           <view class="title">
-            <text class="name">宠物用品</text>
+            <text class="name">{{item.name}}</text>
             <navigator class="more" hover-class="none">全部</navigator>
           </view>
           <view class="section">
-            <navigator v-for="goods in 4" :key="goods" class="goods" hover-class="none" :url="`/pages/goods/goods?id=`">
-              <image class="image" src="https://yanxuan-item.nosdn.127.net/674ec7a88de58a026304983dd049ea69.jpg"></image>
-              <view class="name ellipsis">木天蓼逗猫棍</view>
+            <navigator v-for="goods in item.goods" :key="goods.id" class="goods" hover-class="none" :url="`/pages/goods/goods?id=${goods.id}`">
+              <image class="image" :src="goods.picture"></image>
+              <view class="name ellipsis">{{goods.name}}</view>
               <view class="price">
                 <text class="symbol">¥</text>
-                <text class="number">16.00</text>
+                <text class="number">{{goods.price}}</text>
               </view>
             </navigator>
           </view>
