2252f9447859a229948bbe0e0d3a00c0d6716269
qiaoronggui <903111082@qq.com>
2023-08-14 11:17:35 +0800

    sku模块 打开弹窗修改模式

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -85,6 +85,20 @@ const openPopup = (name: typeof popupName.value) => {
 const isShowSku = ref(false)
 //商品信息
 const localdata = ref({} as SkuPopupLocaldata)
+// 按钮的模式
+enum SkuMode {
+  Both = 1,
+  Cart = 2,
+  Buy = 3
+}
+const mode = ref<SkuMode>(SkuMode.Both)
+// 打开sku弹窗修改按钮模式
+const openSkuPopup = (val: SkuMode) => {
+  // 显示sku组件
+  isShowSku.value = true
+  // 修改按钮模式
+  mode.value = val
+}
 </script>
 
 <template>
@@ -120,7 +134,7 @@ const localdata = ref({} as SkuPopupLocaldata)
 
         <!-- 操作面板 -->
         <view class="action">
-          <view class="item arrow" @tap="isShowSku = true">
+          <view class="item arrow" @tap="openSkuPopup(SkuMode.Both)">
             <text class="label">选择</text>
             <text class="text ellipsis"> 请选择商品规格 </text>
           </view>
@@ -184,8 +198,8 @@ const localdata = ref({} as SkuPopupLocaldata)
         </navigator>
       </view>
       <view class="buttons">
-        <view class="addcart"> 加入购物车 </view>
-        <view class="buynow"> 立即购买 </view>
+        <view class="addcart" @tap="openSkuPopup(SkuMode.Cart)"> 加入购物车 </view>
+        <view class="buynow" @tap="openSkuPopup(SkuMode.Buy)"> 立即购买 </view>
       </view>
     </view>
   </view>
@@ -199,7 +213,7 @@ const localdata = ref({} as SkuPopupLocaldata)
   </uni-popup>
 
   <!-- sku组件 -->
-  <vk-data-goods-sku-popup v-model="isShowSku" :localdata="localdata" />
+  <vk-data-goods-sku-popup v-model="isShowSku" :localdata="localdata" :mode="mode" add-cart-background-color="" />
 </template>
 
 <style lang="scss">
